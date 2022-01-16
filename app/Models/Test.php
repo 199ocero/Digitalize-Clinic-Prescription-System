77adb75085de5d59f7eee190a3b51d51db7723aa
199ocero <57591686+199ocero@users.noreply.github.com>
2022-01-16 22:05:53 +0800

    new changes

--- app/Models/Test.php
+++ app/Models/Test.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Test extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'echocardiogram',
+        'electrocardiogram',
+        'x_ray',
+        'cbc',
+        'urinalysis',
+        'ultrasound',
+        'ct_scan',
+        'stool_test',
+    ];
 }
